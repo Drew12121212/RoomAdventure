aa20b78ec11bc13b9c5dd0d96e89264f45b10a27
Ashish Ghimire <65116983+ashish3542@users.noreply.github.com>
2025-05-22 13:49:41 -0500

    Updated RoomAdventure.java with latest contributions

--- RoomAdventure.java
+++ RoomAdventure.java
@@ -1,24 +1,29 @@
+// RoomAdventure.java
+// Author: Ash
+// Feature Branch: feature-use-command, feature-combine-items
+// Features Added:
+// - "use" command to interact with objects using inventory items
+// - "combine" command to combine two items in inventory
+
 import java.util.Scanner;
 import java.util.HashMap;
 
 public class RoomAdventure {
     private static Room currentRoom;
-    private static String[] inventory = {null,null,null,null,null};
+    private static String[] inventory = {null, null, null, null, null};
     private static String status;
     private static boolean puzzleSolved = false;
     private static boolean running = true;
 
-    final private static String DEFAULT_STATUS = 
-        "Sorry, I do not understand. Try [verb] [noun]. Valid verbs include 'go', 'look', 'eat', 'answer', 'talk' and 'take'.";
-    
+    final private static String DEFAULT_STATUS =
+        "Sorry, I do not understand. Try [verb] [noun]. Valid verbs include 'go', 'look', 'eat', 'answer', 'talk', 'take', 'use', and 'combine'.";
+
     private static void handleGo(String noun) {
         HashMap<String, Room> exitHashMap = currentRoom.getExits();
         status = "I don't see that room";
 
         if (exitHashMap.containsKey(noun)) {
             Room nextRoom = exitHashMap.get(noun);
-
-            //if in Riddle Room and puzzle not solved, block leaving
             if (currentRoom.getName().equals("Riddle Room") && !puzzleSolved) {
                 status = "You are stuck here until you answer the riddle.";
             } else {
@@ -28,16 +33,14 @@ public class RoomAdventure {
         }
     }
 
-
     private static void handleLook(String noun) {
-        HashMap<String,String> items = currentRoom.itemsHashMap;
+        HashMap<String, String> items = currentRoom.itemsHashMap;
         Edible[] edibles = currentRoom.getEdibles();
         status = "I don't see that item";
         if (items.containsKey(noun)) {
             status = items.get(noun);
-        }
-        else {
-            for (Edible item: edibles) {
+        } else {
+            for (Edible item : edibles) {
                 if (noun.equals(item.edibleName)) {
                     status = item.edibleDescription;
                 }
@@ -45,38 +48,32 @@ public class RoomAdventure {
         }
     }
 
-
-    //feature added by aayusha
     private static void handleTalk(String noun) {
         status = "There's no one to talk to.";
-
         if (noun.equals("person") && currentRoom.getItemsHashMap().containsKey("person")) {
             switch (currentRoom.getName()) {
                 case "Labratory":
-                    status = "The person stares blankly and whispers: 'You must solve the puzzle to escape...'";
+                    status = "The person whispers: 'You must solve the puzzle to escape...'";
                     break;
                 case "Armory":
-                    status = "The person smiles softly: 'You found me... I’ve been waiting.'";
+                    status = "The person smiles: 'You found me... I've been waiting.'";
                     break;
                 default:
                     status = "The person blinks silently. No response.";
             }
-        } else if (noun.equals("person")) {
-            status = "Theres no person here to talk to.";
         }
     }
-
 
     private static void handleTake(String noun) {
         String[] grabbables = currentRoom.getGrabbables();
         status = "I can't grab that";
         for (String item : grabbables) {
             if (noun.equals(item)) {
-                for (int i = 0; i <inventory.length; i ++) {
+                for (int i = 0; i < inventory.length; i++) {
                     if (inventory[i] == null) {
                         inventory[i] = noun;
                         status = "Added it to the inventory";
-                        break;
+                        return;
                     }
                 }
             }
@@ -87,15 +84,13 @@ public class RoomAdventure {
         Edible[] edibles = currentRoom.getEdibles();
         handleTake(noun);
         if (status.equals("I can't grab that")) {
-            for (Edible item: edibles) {
+            for (Edible item : edibles) {
                 if (noun.equals(item.edibleName)) {
                     status = item.edibleMessage;
                     Globals.health += item.healthChange;
-                    Edible[] emptyEdible = new Edible[0];
-                    currentRoom.addEdibles(emptyEdible);
+                    currentRoom.addEdibles(new Edible[0]);
                 }
             }
-
         } else {
             status = "I can't eat that but I added it to the inventory";
         }
@@ -114,9 +109,40 @@ public class RoomAdventure {
         }
     }
 
+    private static void handleUse(String noun) {
+        status = "You can't use that.";
+        for (String item : inventory) {
+            if (item != null && noun.equals(item)) {
+                if (noun.equals("key") && currentRoom.getName().equals("Labratory")) {
+                    currentRoom.addItem("note", "A glowing note that reads: 'The answer is echo.'");
+                    status = "You used the key to unlock a drawer. A glowing note appears!";
+                } else if (noun.equals("coal") && currentRoom.getName().equals("Riddle Room")) {
+                    status = "You used the coal on the tablet: '...without a mouth...' appears.";
+                } else {
+                    status = "You used the " + noun + ", but nothing happened.";
+                }
+                return;
+            }
+        }
+    }
 
-    private static String redText(String text) {
-        return Globals.RED + text + Globals.RESET;
+    private static void handleCombine(String noun1, String noun2) {
+        int firstIndex = -1, secondIndex = -1;
+        for (int i = 0; i < inventory.length; i++) {
+            if (noun1.equals(inventory[i])) firstIndex = i;
+            if (noun2.equals(inventory[i])) secondIndex = i;
+        }
+        if (firstIndex != -1 && secondIndex != -1) {
+            if ((noun1.equals("key") && noun2.equals("coal")) || (noun1.equals("coal") && noun2.equals("key"))) {
+                inventory[firstIndex] = "firekey";
+                inventory[secondIndex] = null;
+                status = "You combined key and coal into a firekey!";
+            } else {
+                status = "Those items can't be combined.";
+            }
+        } else {
+            status = "You don't have both items.";
+        }
     }
 
     private static void setupGame() {
@@ -126,182 +152,107 @@ public class RoomAdventure {
         Room labratory = new Room("Labratory");
         Room riddleRoom = new Room("Riddle Room");
 
-        String[] livingRoomGrabbables = {"key"};
-        Edible chickenLeg = new Edible("chicken", "a good looking grilled chicken leg", "you ate the chicken leg and feel restored", 20);
-        Edible[] edibles1 = {chickenLeg};
-        livingRoom.addEdibles(edibles1);
+        livingRoom.addItem("desk", "Desk with a shiny key");
+        livingRoom.setGrabbables(new String[]{"key"});
         livingRoom.addExit("east", foyer);
-        livingRoom.addExit("north", armory);
-        livingRoom.addItem("chair", "It is a chair with a plate of " + redText("chicken") + " on it");
-        livingRoom.addItem("desk", "there is an old wooden desk with a " + redText("key") + " on top");
-        livingRoom.setGrabbables(livingRoomGrabbables);
 
-        String[] foyerGrabbables = {"coal"};
+        foyer.addItem("rug", "Rug hiding a lump of coal");
+        foyer.setGrabbables(new String[]{"coal"});
         foyer.addExit("west", livingRoom);
         foyer.addExit("north", labratory);
-        foyer.addItem("fireplace", "it is on fire");
-        foyer.addItem("rug", "There is a lump of " + redText("coal") + " on the rug");
-        foyer.setGrabbables(foyerGrabbables);
 
-
-        String[] armoryGrabbables = {"sword"};
-        Edible beefPatty = new Edible("patty", "a old moldy beef patty", "You at the beef patty and instantly feel sick", -50);
-        Edible[] edibles3 = {beefPatty};
-        armory.addExit("south", livingRoom);
-        armory.addExit("east", labratory);
-        armory.addItem("wall", "A solid stone wall with a " + redText("patty") + " stuck on it");
-        armory.addItem("stand", "An ornate stand with a foreign " + redText("sword") + " layed upon in");
-        armory.setGrabbables(armoryGrabbables);
-        armory.addEdibles(edibles3);
-
-
-        String[] labratoryGrabbables = {"microscope"};
+        labratory.addItem("cabinet", "Cabinet with a microscope");
+        labratory.setGrabbables(new String[]{"microscope"});
         labratory.addExit("south", foyer);
         labratory.addExit("west", armory);
-        labratory.addItem("cabinent", "There is a chemical cabinent with a " + redText("microscope") + " in it");
-        labratory.addItem("person", "there is someone staring at you in the room");
-        labratory.setGrabbables(labratoryGrabbables);
 
-        //adding riddle room by aayusha
-        riddleRoom.addItem("tablet", "There is an ancient stone tablet with an inscription: 'I speak without a mouth and hear without ears. What am I?'");
-        riddleRoom.setGrabbables(new String[]{});
-        //connecting riddle room to room 3
-        armory.addExit("north", riddleRoom);         
+        armory.addItem("stand", "Sword on a stand");
+        armory.setGrabbables(new String[]{"sword"});
+        armory.addExit("east", labratory);
+        armory.addExit("north", riddleRoom);
+        armory.addExit("south", livingRoom);
+
+        riddleRoom.addItem("tablet", "Tablet with a riddle");
         riddleRoom.addExit("south", armory);
+
         currentRoom = livingRoom;
     }
 
-    @SuppressWarnings("java:S2189")
     public static void main(String[] args) {
         setupGame();
+        Scanner s = new Scanner(System.in);
 
         while (running) {
             System.out.print(currentRoom.toString());
             System.out.print("Inventory: ");
-
-            for (int i = 0; i < inventory.length; i++) {
-                System.out.print(inventory[i] + " ");
-            }
-            
+            for (String item : inventory) System.out.print(item + " ");
             System.out.println("\nWhat would you like to do?");
-            Scanner s = new Scanner(System.in);
             String input = s.nextLine();
-            if (input.equals("quit")) {
-                running = false;
-                s.close();
-                continue;
-            }
-            String[] words = input.split(" ");
+            if (input.equals("quit")) break;
 
-            if (words.length != 2) {
+            String[] words = input.split(" ");
+            if (!(words.length == 2 || (words.length == 3 && words[0].equals("combine")))) {
                 status = DEFAULT_STATUS;
                 System.out.println(status);
                 continue;
             }
-
             String verb = words[0];
-            String noun = words[1];
 
             switch (verb) {
-                case "go":
-                    handleGo(noun);
-                    break;
-                case "look": 
-                    handleLook(noun);
-                    break;
-                case "take":
-                    handleTake(noun);
-                    break;
-                case "talk":
-                    handleTalk(noun);
-                    break;
-                case "answer":
-                    handleAnswer(noun);
-                    break;
-                case "eat":
-                    handleEat(noun);
-                    break;
-                default:
-                    status = DEFAULT_STATUS;
+                case "go": handleGo(words[1]); break;
+                case "look": handleLook(words[1]); break;
+                case "take": handleTake(words[1]); break;
+                case "talk": handleTalk(words[1]); break;
+                case "answer": handleAnswer(words[1]); break;
+                case "eat": handleEat(words[1]); break;
+                case "use": handleUse(words[1]); break;
+                case "combine": handleCombine(words[1], words[2]); break;
+                default: status = DEFAULT_STATUS;
             }
-
             System.out.println(status);
         }
+        s.close();
     }
 }
 
 class Room {
     private String name;
     private String[] grabbables;
-    HashMap<String, Room> exitHashMap = new HashMap<String, Room>();
-    HashMap<String, String> itemsHashMap = new HashMap<String, String>();
-    private Edible[] edibles;
-    
-    public Room(String name) {
-        this.name = name;
-    }
+    HashMap<String, Room> exitHashMap = new HashMap<>();
+    HashMap<String, String> itemsHashMap = new HashMap<>();
+    private Edible[] edibles = new Edible[0];
 
-    public String getName() {
-    return name;
-    }
+    public Room(String name) { this.name = name; }
+    public String getName() { return name; }
+    public void addItem(String item, String desc) { itemsHashMap.put(item, desc); }
+    public void addExit(String dir, Room room) { exitHashMap.put(dir, room); }
+    public HashMap<String, Room> getExits() { return exitHashMap; }
+    public HashMap<String, String> getItemsHashMap() { return itemsHashMap; }
+    public void setGrabbables(String[] g) { this.grabbables = g; }
+    public String[] getGrabbables() { return grabbables; }
+    public void addEdibles(Edible[] edibles) { this.edibles = edibles; }
+    public Edible[] getEdibles() { return edibles; }
 
-    public void addEdibles(Edible[] edibles) {
-        this.edibles = edibles;
-    }
-
-    public Edible[] getEdibles() {
-        return this.edibles;
-    }
-
-    public void addExit(String direction, Room roomName) {
-        exitHashMap.put(direction, roomName);
-    }
-
-    public HashMap<String, Room> getExits() {
-        return exitHashMap;
-    }
-    public void addItem(String item, String itemDescription) {
-        itemsHashMap.put(item, itemDescription);
-    }
-
-    public HashMap<String, String> getItemsHashMap() {
-        return itemsHashMap;
-    }
-
-    public void setGrabbables(String[] grabbables) {
-        this.grabbables = grabbables;
-    }
-
-    public String[] getGrabbables() {
-        return grabbables;
-    }
-
-
-    @Override
     public String toString() {
-        String result = "\nLocation:" + name;
-        result += "\nYou have " + Globals.health + " remaining" ;
-        result += "\nYou See ";
-        for (String item: itemsHashMap.keySet()) {
-            result +=item + " ";
-        }
-        result += "\nExits ";
-        for (String exit: exitHashMap.keySet()) {
-            result += exit + " ";
-        }
-        return result + "\n";
+        String res = "\nLocation: " + name + "\nYou see: ";
+        for (String item : itemsHashMap.keySet()) res += item + " ";
+        res += "\nExits: ";
+        for (String exit : exitHashMap.keySet()) res += exit + " ";
+        return res + "\n";
     }
 }
 
 class Edible {
-        public String edibleName;
-        public String edibleDescription;
-        public String edibleMessage;
-        public int healthChange;
-        public Edible(String edibleName, String edibleDescription, String edibleMessage, int healthChange) {
-            this.edibleName = edibleName;
-            this.edibleDescription = edibleDescription;
-            this.edibleMessage = edibleMessage;
-            this.healthChange = healthChange;
-        }
+    public String edibleName, edibleDescription, edibleMessage;
+    public int healthChange;
+    public Edible(String n, String d, String m, int h) {
+        this.edibleName = n; this.edibleDescription = d;
+        this.edibleMessage = m; this.healthChange = h;
     }
+}
+
+class Globals {
+    public static int health = 100;
+    public static final String RED = "\u001B[31m";
+    public static final String RESET = "\u001B[0m";
+}
